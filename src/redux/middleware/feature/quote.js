a2e86130725e01e32a9f24958fe05b4b9be6ce63
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-11 18:26:22 +0200

    set spinner when loading data & finish fetch

--- src/redux/middleware/feature/quote.js
+++ src/redux/middleware/feature/quote.js
@@ -1,29 +1,33 @@
+import { setLoader } from './../../actions/ui';
 import { apiRequest, API_SUCCESS } from './../../actions/api';
 import { FETCH_QUOTE, QUOTE, setQuote } from './../../actions/quote';
 
-const QUOTE_URL = 'https://goquotes-api.herokuapp.com/api/v1/random?count=1'
+const QUOTE_URL = 'https://goquotes-api.herokuapp.com/api/v1/random?count=1';
 
-export const quoteMiddleware = () => next => action =>  {
-   // to keep action log in order
-   next(action)
+export const quoteMiddleware = () => next => action => {
+  // to keep action log in order
+  next(action);
 
-   switch (action.type) {
+  switch (action.type) {
+    case FETCH_QUOTE:
+      next([
+        apiRequest({
+          body: null,
+          method: 'GET',
+          url: QUOTE_URL,
+          feature: QUOTE,
+        }),
+        setLoader({ state: true, feature: QUOTE }),
+      ]);
+      break;
 
-      case FETCH_QUOTE:
-         next(apiRequest({
-            body: null,
-            method: 'GET',
-            url: QUOTE_URL,
-            feature: QUOTE
-         }))
-         break;
-      
+    case `${QUOTE} ${API_SUCCESS}`:
+      next([
+        setQuote({ quote: action.payload.quotes, normalizeKey: 'author' }),
+        setLoader({ state: false, feature: QUOTE }),
+      ]);
+      break;
 
-      case `${QUOTE} ${API_SUCCESS}`:
-         next(setQuote({ quote: action.payload.quotes, normalizeKey: 'author' }));
-         break;
-
-         default:
-
-   }
-}
+    default:
+  }
+};
