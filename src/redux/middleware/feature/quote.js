ded05532e40333ad2546eefcf83e5b2fa2b5255d
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-11 13:08:48 +0200

    call api request action creator

--- src/redux/middleware/feature/quote.js
+++ src/redux/middleware/feature/quote.js
@@ -1,5 +1,5 @@
-import { FETCH_QUOTE } from './../../actions/quote';
-
+import { apiRequest } from './../../actions/api';
+import { FETCH_QUOTE, QUOTE } from './../../actions/quote';
 
 const QUOTE_URL = 'https://goquotes-api.herokuapp.com/api/v1/random?count=1'
 
@@ -9,6 +9,13 @@ export const quoteMiddleware = () => next => action =>  {
 
    switch (action.type) {
       case FETCH_QUOTE:{
+
+         next(apiRequest({
+            body: null,
+            method: 'GET',
+            url: QUOTE_URL,
+            feature: QUOTE
+         }))
         
          
       }
