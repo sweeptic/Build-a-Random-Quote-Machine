7182adcb4c9c3c452a450c58db4882aa58843d21
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-11 17:24:49 +0200

    dispatch datanormalized action. if any one interested..

--- src/redux/middleware/core/normalize.js
+++ src/redux/middleware/core/normalize.js
@@ -1,13 +1,18 @@
+import { dataNormalized } from './../../actions/data';
 import { setQuote } from './../../actions/quote';
 
 export const normalizeMiddleware = ({dispatch}) => next => action => {
 
    if(action.type.includes('SET') && action.meta.normalizeKey){
      
+      
       const quote =  action.payload.reduce((acc, item) => {
          acc[item[action.meta.normalizeKey]] = item.text
          return acc;
       }, {})
+
+      
+      dispatch(dataNormalized({ feature: action.meta.feature }));
 
       next(setQuote({quote, normalizeKey: null}))
 
