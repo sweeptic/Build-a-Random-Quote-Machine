6ec80fade76341b5f8c39565c1473f55f349017f
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-11 12:21:23 +0200

    add fetch quote action creator

---
+++ src/redux/actions/quote.js
@@ -0,0 +1,12 @@
+import { FETCH_QUOTE } from './quote';
+// feature name
+export const QUOTE = '[Quote]';
+
+//action types
+export const FETCH_QUOTE = `${QUOTE} FETCH`;
+
+//action creators
+export const fetchQuote = ({ query }) => ({
+  type: FETCH_QUOTE,
+  payload: query,
+});
