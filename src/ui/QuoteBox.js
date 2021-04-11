fa179cc7b976e780a919dc856513bcad7aa19ef4
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-11 17:13:20 +0200

    add depedency to useffect hook

--- src/ui/QuoteBox.js
+++ src/ui/QuoteBox.js
@@ -1,37 +1,50 @@
-import {  getSingleQuoteAuthor, getSingleQuoteText } from './../redux/reducers/quote';
-import {  useDispatch } from 'react-redux';
+import { getSingleQuoteAuthor, getSingleQuoteText } from './../redux/reducers/quote';
+import { useDispatch } from 'react-redux';
 import { fetchQuote } from './../redux/actions/quote';
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const QuoteBox = () => {
-  
-    const QuoteText = useSelector(state => getSingleQuoteText(state))
-    const QuoteAuthor = useSelector(state => getSingleQuoteAuthor(state))
-    const dispatch = useDispatch()
-    
-    useEffect(() => {
-     dispatch(fetchQuote(''));
-    }, []);
+  const QuoteText = useSelector(state => getSingleQuoteText(state));
+  const QuoteAuthor = useSelector(state => getSingleQuoteAuthor(state));
+  const dispatch = useDispatch();
 
-
+  useEffect(() => {
+    dispatch(fetchQuote(''));
+  }, [dispatch]);
 
   return (
     <div id='quote-box' className='col-sm-6 mx-auto'>
-
       <h1 className='mb-5'>Quote generator</h1>
 
       <div className='card'>
-        <span className='card-header' id='author'>{QuoteAuthor}</span>
+        <span className='card-header' id='author'>
+          {QuoteAuthor}
+        </span>
         <div className='card-body'>
-          <p className='card-text' id='text'>{QuoteText}</p>
-          <button onClick={() => { dispatch(fetchQuote('')) }} className='btn btn-primary m-2' id='new-quote'>new quote</button>
-          <a className='btn btn-secondary m-2'  href='https://twitter.com/intent/tweet' target="_blank"  id='tweet-quote'>tweet quote</a>
+          <p className='card-text' id='text'>
+            {QuoteText}
+          </p>
+          <button
+            onClick={() => {
+              dispatch(fetchQuote(''));
+            }}
+            className='btn btn-primary m-2'
+            id='new-quote'>
+            new quote
+          </button>
+          <a
+            className='btn btn-secondary m-2'
+            href='https://twitter.com/intent/tweet'
+            target='_blank'
+            id='tweet-quote'
+            rel='noreferrer'>
+            tweet quote
+          </a>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default QuoteBox;
