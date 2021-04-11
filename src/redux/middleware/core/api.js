7b46cb43f68b47ac5fb2f70d6235a1de4b74e954
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-11 13:13:23 +0200

    handle API_REQUEST action creator and call additional actions later

--- src/redux/middleware/core/api.js
+++ src/redux/middleware/core/api.js
@@ -1,3 +1,17 @@
+import { API_REQUEST } from './../../actions/api';
+
 export const apiMiddleware = ({dispatch}) => next => action => {
    next(action);
+
+   if (action.type.includes(API_REQUEST)) {
+      const { body, url, method, feature } = action.meta;
+      // feature - integrity key
+  
+      //mapping an action to a different
+      // action while maintaining integrity using the feature name.
+      fetch(url, { body, method })
+        .then(response => response.json())
+      //   .then(response => dispatch(apiSuccess({ response, feature })))
+      //   .catch(error => dispatch(apiError({ error, feature })));
+    }
 }
