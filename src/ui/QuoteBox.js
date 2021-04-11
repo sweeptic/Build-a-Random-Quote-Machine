5997c051aa64c88ca41cac8d3943aeeba7413887
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-04-11 12:36:42 +0200

    format quotebox.js

--- src/ui/QuoteBox.js
+++ src/ui/QuoteBox.js
@@ -9,35 +9,19 @@ const QuoteBox = props => {
 
   return (
     <div id='quote-box' className='col-sm-6 my-5'>
+
       <h1 className='mb-5'>Quote generator</h1>
 
       <div className='card'>
-        <span className='card-header' id='author'>
-          author
-        </span>
-
+        <span className='card-header' id='author'>author</span>
         <div className='card-body'>
-          <p className='card-text' id='text'>
-            text
-          </p>
-          <button
-            onClick={() => {
-              props.fetchQuote('');
-            }}
-            className='btn btn-primary m-2'
-            id='new-quote'
-          >
-            new quote
-          </button>
-          <a
-            className='btn btn-secondary m-2'
-            href='https://twitter.com/intent/tweet'
-            id='tweet-quote'
-          >
-            tweet quote
-          </a>
+          <p className='card-text' id='text'>text</p>
+          <button onClick={() => { props.fetchQuote(''); }} className='btn btn-primary m-2' id='new-quote'>new quote</button>
+          <a className='btn btn-secondary m-2' href='https://twitter.com/intent/tweet' id='tweet-quote'>tweet quote</a>
         </div>
       </div>
+
+
     </div>
   );
 };
